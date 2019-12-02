edf63c783c6c943f8c84a065721d932758aa0de4
mediateri <ntawukuriryayomeddy@gmail.com>
2019-12-02 15:49:11 +0000

    changed name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :content, presence: true
+  validates :content, length: {maximum: 255}
   belongs_to :announcement
   def set_defaults_before_save
     self.my_boolean_field ||= false
